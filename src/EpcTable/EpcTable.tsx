fe8ca47f3c8e8cb5265271f7ec394c5cc108e866
BangZ <i@bangz.me>
2022-08-18 11:08:53 +0800

    change opeartion size

--- src/EpcTable/EpcTable.tsx
+++ src/EpcTable/EpcTable.tsx
@@ -96,7 +96,7 @@ export const EpcTable: FC<{
 }> = (props) => {
   const columns: ColumnsType<PartsTableData> = !isEmpty(props.partsListConfigs)
     ? props.partsListConfigs
-      .concat({ field: 'action', title: '操作', headerStyle: { width: 150 } })
+      .concat({ field: 'action', title: '操作' })
       .map<ColumnType<PartsTableData>>(conf => ({
         title: conf.title,
         dataIndex: conf.field,
